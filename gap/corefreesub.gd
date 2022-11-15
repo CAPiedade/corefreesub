b198946c0a9910c4ba4e6c0a5d185455a38491c6
CAPiedade <elsivier@hotmail.com>
2022-11-15 15:25:03 +0000

    Doc build, documentation in .gd and new functions

--- gap/corefreesub.gd
+++ gap/corefreesub.gd
@@ -1,22 +1,96 @@
-#
-# corefreesub: A GAP Package for calculating the core-free subgroups and their faithful transitive permutation representations
-#
+########################################################################
+##
+##  CoreFreeSub package
+##
+##  Copyright 2022
+##    A GAP Package for calculating the core-free subgroups and their faithful transitive permutation representations
+##    
+##    
+##
+## Licensed under the GPL 3 or later.
+##
+########################################################################
+# 
 #! @Chapter Introduction
 #!
-#! corefreesub is a package which does some
-#! interesting and cool things
+#! corefreesub is a package which calculates the core-free subgroups and their faithful transitive permutation representations
+#! 
 #!
-#! @Chapter Functionality
-#!
-#!
-#! @Section Example Methods
-#!
-#! This section will describe the example
-#! methods of corefreesub
-
+#! @Chapter Core Free Subgroups
+#
+#! @Section IsCoreFree
+#! 
+#! @Arguments G, H
+#! @Returns a boolean
 #! @Description
-#!   Insert documentation for your function here
-DeclareGlobalFunction( "CoreFreeConjugacyClassesSubgroups" );
-DeclareGlobalFunction( "AllCoreFreeSubgroups" );
-DeclareGlobalFunction( "CoreFreeDegrees" );
+#!  Returns whether the subgroup <A>H</A> is core-free in its parent group <A>G</A>
+#! @BeginExampleSession
+#! gap> LoadPackage("CoreFreeSub");
+#! gap> G := SymmetricGroup(4);; H := Subgroup(G, [(1,3)(2,4)]);;
+#! gap> Core(G,H);
+#! Group(())
+#! gap> IsCoreFree(G,H);
+#! true
+#! gap> H := Subgroup(G, [(1,4)(2,3), (1,3)(2,4)]);;
+#! gap> IsCoreFree(G,H);
+#! false
+#! gap> Core(G,H); # H is a normal subgroup of G, hence it does not have a trivial core
+#! Group([ (1,4)(2,3), (1,3)(2,4) ])
+#! @EndExampleSession
 DeclareGlobalFunction( "IsCoreFree" );
+
+#! @Section CoreFreeConjugacyClassesSubgroups
+#! 
+#! @Arguments G
+#! @Returns a list
+#! @Description
+#!  Returns a list of all conjugacy classes of core-free subgroups of <A>G</A>
+#! @BeginExampleSession
+#! gap> LoadPackage("CoreFreeSub");
+#! gap> G := SymmetricGroup(4);; dh := DihedralGroup(10);;
+#! gap> CoreFreeConjugacyClassesSubgroups(G);
+#! [ Group( () )^G, Group( [ (1,3)(2,4) ] )^G, Group( [ (3,4) ] )^G, Group( [ (2,4,3) ] )^G, Group( [ (1,4)(2,3), (1,3)(2,4) ] )^G, Group( [ (3,4), (1,2)(3,4) ] )^G, Group( [ (1,3,2,4), (1,2)(3,4) ] )^G, Group( [ (3,4), (2,4,3) ] )^G, Group( [ (1,4)(2,3), (1,3)(2,4), (3,4) ] )^G, Group( [ (1,4)(2,3), (1,3)(2,4), (2,4,3) ] )^G, Group( [ (1,4)(2,3), (1,3)(2,4), (2,4,3), (3,4) ] )^G ]
+#! gap> CoreFreeConjugacyClassesSubgroups(dh);
+#! [ Group( <identity> of ... )^G, Group( [ f1 ] )^G ] 
+#!@EndExampleSession
+DeclareGlobalFunction( "CoreFreeConjugacyClassesSubgroups" );
+
+#! @Section AllCoreFreeSubgroups
+#! 
+#! @Arguments G
+#! @Returns a list
+#! @Description
+#!  Returns a list of all core-free subgroups of <A>G</A>
+#! @BeginExampleSession
+#! gap> LoadPackage("CoreFreeSub");
+#! gap> G := SymmetricGroup(4);; dh := DihedralGroup(10);;
+#! gap> AllCoreFreeSubgroups(G);
+#! [ Group(()), Group([ (1,3)(2,4) ]), Group([ (1,4)(2,3) ]), Group([ (1,2)(3,4) ]), Group([ (3,4) ]), Group([ (2,4) ]), Group([ (2,3) ]), Group([ (1,4) ]), Group([ (1,3) ]), Group([ (1,2) ]), Group([ (2,4,3) ]), Group([ (1,3,2) ]), Group([ (1,3,4) ]), Group([ (1,4,2) ]), Group([ (3,4), (1,2)(3,4) ]), Group([ (2,4), (1,3)(2,4) ]), Group([ (2,3), (1,4)(2,3) ]), Group([ (1,3,2,4), (1,2)(3,4) ]), Group([ (1,2,3,4), (1,3)(2,4) ]), Group([ (1,2,4,3), (1,4)(2,3) ]), Group([ (3,4), (2,4,3) ]), Group([ (1,3), (1,3,2) ]), Group([ (1,3), (1,3,4) ]), Group([ (1,4), (1,4,2) ]) ]
+#! gap> AllCoreFreeSubgroups(dh);
+#! [ Group([  ]), Group([ f1 ]), Group([ f1*f2^2 ]), Group([ f1*f2^4 ]), Group([ f1*f2 ]), Group([ f1*f2^3 ]) ]
+#!@EndExampleSession
+DeclareGlobalFunction( "AllCoreFreeSubgroups" );
+
+#! @Section CoreFreeDegrees
+#! 
+#! @Arguments G
+#! @Returns a list
+#! @Description
+#!  Returns a list of all possible degrees of faithful transitive permutation representations of <A>G</A>. The degrees of a faithful transitive permutation representation of <A>G</A> are the index of its core-free subgroups.
+#! @BeginExampleSession
+#! gap> LoadPackage("CoreFreeSub");
+#! gap> G := SymmetricGroup(4);; dh := DihedralGroup(10);;
+#! gap> CoreFreeDegrees(G);
+#! [ 24, 12, 8, 6, 4 ]
+#! gap> CoreFreeDegrees(dh);
+#! [10, 5]
+#! @EndExampleSession
+DeclareGlobalFunction( "CoreFreeDegrees" );
+
+DeclareOperation( "MinimalFaithfulTransitivePermutationRepresentation", [IsGroup]);
+
+
+DeclareGlobalFunction( "MinimalFaithfulTransitivePermutationRepresentationDegree" );
+
+
+
--- gap/corefreesub.gi
+++ gap/corefreesub.gi
@@ -16,17 +16,45 @@ function(G)
 		H := Image(iso);
 	else
 		H := G;
+		iso := IdentityMapping(G);
 	fi;
 	return List(Filtered(ConjugacyClassesSubgroups(H), n -> IsCoreFree(H,n[1])), i -> ConjugacyClassSubgroups(G,PreImage(iso,Representative(i))) );
 end );
 
 InstallGlobalFunction( AllCoreFreeSubgroups,
 function(G)
+	local i, j;
 	return Flat(List(CoreFreeConjugacyClassesSubgroups(G), i -> List(i, j -> j)));
 end );
 
 InstallGlobalFunction( CoreFreeDegrees,
 function(G)
-	local subgroups;
-	return Unique(List(CoreFreeConjugacyClassesSubgroups(G), n -> Index(G,n[1]));
+	local n;
+	return Unique(List(CoreFreeConjugacyClassesSubgroups(G), n -> Index(G,n[1])));
 end );
+
+
+InstallMethod( MinimalFaithfulTransitivePermutationRepresentation, [IsGroup], 
+function(G)
+	return Image(FactorCosetAction(G,Reversed(CoreFreeConjugacyClassesSubgroups(G))[1][1]));
+end );
+
+InstallOtherMethod( MinimalFaithfulTransitivePermutationRepresentation, [IsGroup, IsBool], 
+function(G,bool)
+	local core_free_ccs;
+	if bool then
+		core_free_ccs := Reversed(CoreFreeConjugacyClassesSubgroups(G));
+		return List(Filtered(core_free_ccs, i -> Index(G,i[1]) = Index(G,core_free_ccs[1][1])), j -> Image(FactorCosetAction(G,j[1])));
+	else
+		return MinimalFaithfulTransitivePermutationRepresentation(G);
+	fi;
+end );
+
+
+InstallGlobalFunction( MinimalFaithfulTransitivePermutationRepresentationDegree,
+function(G)
+	local n;
+	return Minimum(List(CoreFreeConjugacyClassesSubgroups(G), n -> Index(G,n[1])));
+end );
+
+
